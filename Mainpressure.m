a2d49ca197fdc4c5584afc05eb755eedae5baa99
Mahshad Lotfinia <mahshadlotfinia66@gmail.com>
2021-01-26 09:39:42 +0100

    Add files via upload

---
+++ Mainpressure.m
@@ -0,0 +1,104 @@
+clear all
+clc
+close all
+%% 01 - From 5.77 to 0 Backward (and Forward: Uncomment the upper q formula and comment the lower one)
+h1=input('Enter First Thickness: ');
+h2_577=input('Enter Last Thickness: ');%h2
+R_577=input('Enter Radios Of Roller: ');
+phi=acosd(1-((h1-h2_577)/(2*R_577)));
+p_577=input('Enter friction coefficient: ') ;
+strainrate=input('Enter Strain Rate: ');
+fi_577=phi:-0.01:0; %fi
+h_577(1)=h2_577+(2*R_577*(1-cos(pi()*(fi_577(1)/180)))); %h0
+sigma_577(1)=input('Enter Yield Point Stress: ');%sigma0
+k_577(1)=sigma_577(1)/(3^.5); %k0
+q_577(1)=2*(sigma_577(1)/(3^.5)); %q0
+%% ann
+xneural=xlsread('xneural.xlsx');
+yneural=xlsread('yneural.xlsx');
+x = xneural';
+t = yneural';
+trainFcn = 'trainlm'; 
+hiddenLayerSize = 6;
+net = fitnet(hiddenLayerSize,trainFcn);
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+[net,tr] = train(net,x,t);
+y = net(x);
+e = gsubtract(t,y);
+performance = perform(net,t,y);
+view(net);
+%%
+for i=2:numel(fi_577)
+    h_577(i)=h2_577+(2*R_577*(1-cos(pi()*(fi_577(i)/180))));
+    ept_577(i)=log(h_577(i)/h_577(1));
+    epw_577(i)=-ept_577(i);
+    epbar_577(i)=((2/3)*((ept_577(i))^2+(epw_577(i))^2))^.5;
+    
+    sigma_577(i)=net([ epbar_577(i);strainrate]);
+    k_577(i)=sigma_577(i)/((3)^.5);
+%    q_577(i)=(((4*k_577(i-1)*R_577*fi_577(i-1)*(pi()/180))-(2*p_577*R_577*q_577(i-1)))*((fi_577(i)-fi_577(i-1))*(pi()/180))/h_577(i-1))+(2*k_577(i))+q_577(i-1)-(2*k_577(i-1));
+    q_577(i)=(-1)*((((4*k_577(i-1)*R_577*fi_577(i-1)*(pi()/180))-(2*p_577*R_577*q_577(i-1)))*((fi_577(i-1)-fi_577(i))*(pi()/180))/h_577(i-1))+(2*k_577(i-1))-q_577(i-1)-(2*k_577(i)));
+end
+
+%% 02 - From 0 to 5.77 Forward (and Backward: Uncomment the lower q formula and comment the upper one)
+h2_0=h2_577; %h2
+R_0=R_577;
+p_0=p_577;
+fi_0=0:0.01:phi; %fi
+h_0(1)=h2_0+(2*R_0*(1-cos(pi()*(fi_0(1)/180))));%h0
+ept_0(1)=log(h_0(1)/h_577(1));
+epw_0(1)=-ept_0(1);
+epbar_0(1)=((2/3)*((ept_0(1))^2+(epw_0(1))^2))^.5;
+sigma_0(1)=net([epbar_0(1);strainrate]); %sigma0
+k_0(1)=sigma_0(1)/(3^.5); %k0
+q_0(1)=2*(sigma_0(1)/(3^.5)); %q0
+
+for i=2:numel(fi_0)
+    h_0(i)=h2_0+(2*R_0*(1-cos(pi()*(fi_0(i)/180))));
+    ept_0(i)=log(h_0(i)/h_577(1));
+    epw_0(i)=-ept_0(i);
+    epbar_0(i)=((2/3)*((ept_0(i))^2+(epw_0(i))^2))^.5;
+    sigma_0(i)=net([epbar_0(i);strainrate]);
+    k_0(i)=sigma_0(i)/((3)^.5);
+%    q_0(i)=(((4*k_0(i-1)*R_0*fi_0(i-1)*(pi()/180))+(2*p_0*R_0*q_0(i-1)))*((fi_0(i)-fi_0(i-1))*(pi()/180))/h_0(i-1))+(2*k_0(i))+q_0(i-1)-(2*k_0(i-1));
+    q_0(i)=(-1)*((((4*k_0(i-1)*R_0*fi_0(i-1)*(pi()/180))+(2*p_0*R_577*q_0(i-1)))*((fi_0(i-1)-fi_0(i))*(pi()/180))/h_0(i-1))+(2*k_0(i-1))-q_0(i-1)-(2*k_0(i)));
+
+end
+%% mean pressure
+numberq=numel(q_577);
+numberfi=numel(fi_577);
+
+mean=0;
+for mm=1:numberq
+    for nn=1:numberfi
+        mean=mean+q_577(mm).*fi_577(nn);
+    end
+end 
+mean;
+mean_pressure=mean/phi;
+
+
+
+%% 02 - Visualization
+   figure('units','centimeters','outerposition',[5 5 12.5 12.5],'color','w','paperpositionmode','auto','resize','off','visible','on','PaperType','A4','PaperOrientation','Portrait','PaperUnits','Centimeters','PaperPosition',[2.5, 2.5, 10, 18],'PaperPositionMode','auto'); 
+                 set(gca,'fontname','Times New Roman','FontSize',12,'LineWidth',0.8,'box','off','color','none');
+                 xlim([0, 6]);
+                 ylim([0, 2000]);
+                 set(gca,'XAxisLocation','bottom','XDir','normal','XScale','linear','XColor','k','XGrid','on','XMinorTick','on','XMinorGrid','on');
+                 set(gca,'YAxisLocation', 'left' ,'YDir','normal','YScale','linear','YColor','k','YGrid','on','YMinorTick','on','YMinorGrid','on');
+                 set(gca,'xtick',[0, 1.5, 3, 4.5, 6],'xticklabel',{'0.0', '1.5', '3.0', '4.5', '6.0'});
+                 set(gca,'ytick',[0, 500,   1000,    1500,    2000],'yticklabel',{'0',     '500',     '1000',     '1500',     '2000'});
+				 set(gca,'TickDir','in','TickLength',[0.02, 0.01]);
+                 set (gca,'GridLineStyle','-','GridColor','k','GridAlpha',0.6);
+              
+                 set(gca,'MinorGridLineStyle',':','MinorGridColor','k','MinorGridAlpha',0.3); 
+                 xlabel('Fi [Degree]','color','k','fontname','Times New Roman','fontsize',13,'fontweight','bold'); 
+                 ylabel('q [-]','color','k','fontname','Times New Roman','fontsize',13,'fontweight','bold');  
+                 hold on;
+                 P1=plot(fi_577,q_577,'LineStyle','-','color','b','LineWidth',1.5); hold on;
+                 P2=plot(fi_0,q_0,'LineStyle','-','color','r','LineWidth',1.5); hold on;
+                 leg=legend([P1, P2],{'5.77 to 0.00', '0.00 to 5.77'},'Location','northwest','Orientation','vertical','TextColor','k','fontname','Times New Roman','fontsize',13,'box','on','Color','w','EdgeColor','k');
+
+            
---
+++ RollerDeflection.m
@@ -0,0 +1,91 @@
+clc
+clear
+close all
+
+L = 0.2000;
+% E = 210e9;
+E1 = 211e9;
+E2 = 211e9;
+%b = 0.01;
+%h = 0.01;
+r=0.075;
+d=2*r;
+A = pi*(r^2);
+I = (pi*(d^4))/64;
+F0 = input('main pressure');
+
+x1 = 0.075;
+x2 = 0.045;
+
+N = input('number of elements');
+x = linspace(0,L,N);
+l = x(2) - x(1);
+
+NS = floor(x1/l)+1;
+NE = floor((x1+x2)/l)+1;
+
+Ke = [  12/l^3,  6/l^2, -12/l^3,  6/l^2
+         6/l^2,    4/l,  -6/l^2,    2/l
+       -12/l^3, -6/l^2,  12/l^3, -6/l^2
+         6/l^2,    2/l,  -6/l^2,    4/l];
+     
+Fe = l/2*[1
+          0
+          1
+          0];
+
+W = zeros(N,1);
+T = zeros(N,1);
+KT = zeros(2*N,2*N);
+FT = zeros(2*N,1);
+
+for i = 1:N-1
+    
+    E = (E2-E1)/(N-1)*(i-1)+E1;
+    KT(2*i-1:2*(i+1),2*i-1:2*(i+1)) = KT(2*i-1:2*(i+1),2*i-1:2*(i+1)) + E*I*Ke;
+    
+end
+
+for i = NS:NE
+    
+    % FT(2*i-1:2*(i+1),1) = FT(2*i-1:2*(i+1),1) + F0*(i-NS)/(NE-NS)*Fe;
+    FT(2*i-1:2*(i+1),1) = FT(2*i-1:2*(i+1),1) + F0*Fe;
+    
+end
+
+KT(1,:) = 0;
+KT(:,1) = 0;
+KT(1,1) = 1;
+
+KT(2,:) = 0;
+KT(:,2) = 0;
+KT(2,2) = 1;
+
+KT(2*N-1,:) = 0;
+KT(:,2*N-1) = 0;
+KT(2*N-1,2*N-1) = 1;
+
+ KT(2*N,:) = 0;
+ KT(:,2*N) = 0;
+ KT(2*N,2*N) = 1;
+
+X = KT^-1*FT;
+
+for i = 1:N
+    
+    W(i) = X(2*i-1);
+    T(i) = X(2*i);
+    
+end
+
+figure
+plot(x,W)
+grid on
+xlabel('x')
+ylabel('w')
+
+figure
+plot(x,T)
+grid on
+xlabel('x')
+ylabel('Slope')
